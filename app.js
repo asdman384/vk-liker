0bdf2f80a84ea27262c2c933f819fac9a1f8649a
Oleg <asdman87@gmail.com>
2016-06-26 21:28:52 +0300

    Update app.js
    1

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var http = require('http');
-http.get('http://api.ipify.org/?format=json', responseCb);
-function responseCb(resp) {
-    console.log(1);
+
+var feedUrl = 'https://api.vk.com/method/wall.get?owner_id=4098511&count=2&access_token=6ec9727ace46363c52f816f72554286d118b15b0eea8c38d9a2a712242bc4035451e8b6ca687fc0f86aa0&v=5.52'
+
+
+http.get(feedUrl, getFeed);
+
+
+function getFeed(resp) {
     var data = '';
-    resp.on('data',function(ch){console.log('data',ch);data += ch});
-    resp.on('end', function(){console.log(data)});
+    
+    resp.on('data', (ch) => { data += ch; } );
+    resp.on('end', () => { console.log(JSON.parse(data)); });
 };
