1e2be9875e2db0ae8a45d08b22a99d5fd409a312
asdman384 <asdman87@gmail.com>
2016-07-26 20:32:25 +0300

    tab => spaces

--- app.js
+++ app.js
@@ -3,129 +3,128 @@
  * vk auth:
  * 1. https://oauth.vk.com/authorize?client_id=5503631&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=73730&response_type=token&v=5.52
  * 
- */ 
+ */
 var https = require('https');
 var fs = require('fs');
 var timeout = 15; //sec
-var token = fs.readFileSync('token','utf8').replace(/\n/, '');
+var token = fs.readFileSync('token', 'utf8').replace(/\n/, '');
 var exec = require('child_process').exec;
 
 var VK = {
-	
-	protocol: 'https:',
-  	hostname: 'api.vk.com',  	
-  	path: '&access_token=' + token + '&v=5.52',
-  	avatars_album_id: -6,
 
-  	getFeed: function(start_time, end_time, callback) {
+    protocol: 'https:',
+    hostname: 'api.vk.com',
+    path: '&access_token=' + token + '&v=5.52',
+    avatars_album_id: -6,
 
-  		this._doRequest('/method/newsfeed.get?source_ids=list2&filters=post&count=5&end_time=' + end_time + '&start_time=' + start_time + this.path)
-  		.then(callback)
-  		.catch(this._errHandle); 		
+    getFeed: function (start_time, end_time, callback) {
 
-  	},
+        this._doRequest('/method/newsfeed.get?source_ids=list2&filters=post&count=5&end_time=' + end_time + '&start_time=' + start_time + this.path)
+            .then(callback)
+            .catch(this._errHandle);
 
-  	addLike: function(owner_id, item_id, callback){
-  		
-  		this._doRequest('/method/likes.add?type=photo&owner_id=' + owner_id + '&item_id=' + item_id + this.path)
-  		.then(callback)
-  		.catch(this._errHandle);
-  	},
+    },
 
-	_doRequest: function(path) {
-		var params = {protocol: this.protocol, hostname: this.hostname,	path: path};
-		return new Promise(function(resolve, reject){
-			https
-			.get(params, function (resp) {
-				var data = '';
-				resp.on('data', (chunk) => { data += chunk; });
-				resp.on('end', () => { resolve(JSON.parse(data)); });
-			})	
-			.on('error', function(err) {
-				if (err.CODE === 'ENOTFOUND'){
-					reject('lost connection');
-				} else {
-					reject(err);
-				}
-			});
-		});		
-	},
+    addLike: function (owner_id, item_id, callback) {
 
-	_errHandle: function(err){
-		console.log(err);
-	}
+        this._doRequest('/method/likes.add?type=photo&owner_id=' + owner_id + '&item_id=' + item_id + this.path)
+            .then(callback)
+            .catch(this._errHandle);
+    },
+
+    _doRequest: function (path) {
+        var params = {protocol: this.protocol, hostname: this.hostname, path: path};
+        return new Promise(function (resolve, reject) {
+            https
+                .get(params, function (resp) {
+                    var data = '';
+                    resp.on('data', (chunk) => {data += chunk;});
+                    resp.on('end', () => {resolve(JSON.parse(data));});
+                })
+                .on('error', function (err) {
+                    if (err.CODE === 'ENOTFOUND') {
+                        reject('lost connection');
+                    } else {
+                        reject(err);
+                    }
+                });
+        });
+    },
+
+    _errHandle: function (err) {
+        console.log(err);
+    }
 };
 
 
 var scaner = {
 
-	start_time: 0,
-	end_time: 0,
-	last_success_request_time: 0,
-	timeout: 0,
+    start_time: 0,
+    end_time: 0,
+    last_success_request_time: 0,
+    timeout: 0,
 
-	init: function(timeout) {
-		this.timeout = timeout;
-	},
+    init: function (timeout) {
+        this.timeout = timeout;
+    },
 
-	scan: function() {
-		
-		this.end_time = Date.now().toString().slice(0, -3);
-		this.start_time = this.last_success_request_time || (this.end_time - this.timeout);
+    scan: function () {
 
-		//console.log('start=', toTime(this.start_time), ' end=', toTime(this.end_time));
+        this.end_time = Date.now().toString().slice(0, -3);
+        this.start_time = this.last_success_request_time || (this.end_time - this.timeout);
 
-		VK.getFeed(this.start_time, this.end_time, this.findPost.bind(this));
+        //console.log('start=', toTime(this.start_time), ' end=', toTime(this.end_time));
 
-		setTimeout(this.scan.bind(this), this.timeout * 1000);
-	},
+        VK.getFeed(this.start_time, this.end_time, this.findPost.bind(this));
 
-	findPost: function(feed) {
-		
-		if (feed.error) {
-			log(feed.error.error_msg);
-			process.exit();
-		}
+        setTimeout(this.scan.bind(this), this.timeout * 1000);
+    },
 
-		this.last_success_request_time = this.end_time;
+    findPost: function (feed) {
 
-		feed.response.items.map(function(item) {
-			
-			log(item);
-			
-			if (item.post_source.data === "profile_photo" ) {
-				var photo = item.attachments[0].photo;
-                		var profile = feed.response.profiles.find(item => item.id === photo.owner_id);
-                		
-				VK.addLike(photo.owner_id, photo.id, log);
-				log(
-		                    profile.first_name + '_' + profile.last_name                    
-		                    , 'likes_count:' + (item.likes.count + 1)
-		                    , photo.photo_1280 || photo.photo_807 || photo.photo_604
-		                );
-			}
-		});
-	}
+        if (feed.error) {
+            log(feed.error.error_msg);
+            process.exit();
+        }
+
+        this.last_success_request_time = this.end_time;
+
+        feed.response.items.map(function (item) {
+
+            if (item.post_source.data === "profile_photo") {
+                var photo = item.attachments[0].photo;
+                var profile = feed.response.profiles.find(item => item.id === photo.owner_id);
+
+                VK.addLike(photo.owner_id, photo.id, log);
+                log(
+                    toTime(this.end_time)
+                    , profile.first_name + '_' + profile.last_name
+                    , 'likes_count:' + (item.likes.count + 1)
+                    , photo.photo_1280 || photo.photo_807 || photo.photo_604
+                );
+            }
+        });
+    }
 };
 
 var Phone = {
 
-    notify: function(title, text, url) {
-        exec('termux-notification -c ' + text + ' -t ' + title + ' -u ' + url);        
+    notify: function (title, text, url) {
+        exec('termux-notification -c ' + text + ' -t ' + title + ' -u ' + url);
     }
 
 }
 
 
 function log(msg) {
-	console.log (msg);
+    console.log(msg);
 }
 
 function toTime(unixtimestamp) {
-    var dateObj = new Date(unixtimestamp *1000);
-    return  ('0' + dateObj.getHours()).slice(-2) + ':' + 
-            ('0' + dateObj.getMinutes()).slice(-2) + ':' + 
-            ('0' + dateObj.getSeconds()).slice(-2);
+    var dateObj = new Date(unixtimestamp * 1000);
+    return ('0' + dateObj.getHours()).slice(-2) + ':' +
+        ('0' + dateObj.getMinutes()).slice(-2) + ':' +
+        ('0' + dateObj.getSeconds()).slice(-2);
 }
 
 scaner.init(timeout)
